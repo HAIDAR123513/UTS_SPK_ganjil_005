246c668a53d438f8ebc9e0a4084b19ba836a1c08
HAIDAR GUHARDY M <56230276+HAIDAR123513@users.noreply.github.com>
2021-10-26 07:18:46 +0700

    files upload

---
+++ UTS SPK/utsspk/page/beranda.php
@@ -0,0 +1,19 @@
+<!-- judul -->
+<div class="panel">
+    <div class="panel-middle" id="judul">
+        <img src="asset/image/home_black.png" style="height: 45px">
+        <div id="judul-text">
+            <h2 class="text-black">BERANDA</h2>
+            Halamanan Utama Administrator
+        </div>
+    </div>
+</div>
+<!-- judul -->
+<div class="panel">
+    <div class="panel-middle text-center">
+        <h1>
+            Selamat Datang di Sistem Pendukung Keputusan pemilihan supplier berbasis web menggunakan metode <span class="text-green">Simple Additive Weighting (SAW)</span>
+        </h1>
+    </div>
+    <div class="panel-bottom"></div>
+</div>
